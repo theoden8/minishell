2d418053ff30535848ca2093b7f1dc9a9d19dd1b
Kirill Rodriguez <theoden8@gmail.com>
2017-12-07 00:41:03 +0000

    random -> urandom

--- random.c
+++ random.c
@@ -6,7 +6,7 @@
 
 // set seed using /dev/random
 void seed_rng() {
-  int fd=open("/dev/random", O_RDONLY);
+  int fd=open("/dev/urandom", O_RDONLY);
   if(fd==-1) abort();
   unsigned seed,pos = 0;
   while(pos<sizeof(seed)){int a=read(fd,(char*)&seed+pos,sizeof(seed)-pos);if(a<=0)abort();pos += a;}
